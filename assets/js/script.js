c398db6a5ecac35d77c23ca91f97d4daa5a8b1c3
tkcannon <taylorkcannon@gmail.com>
2021-12-03 16:22:36 -0700

    _

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-var city = {
-    name: "",
-    lat: "",
-    lon: ""
-}
-
 var searchHistory = {};
 
 function getHistory() {
@@ -24,6 +18,7 @@ function getHistory() {
         }
     }
 
+    $("#history").empty();
     for (i = 0; i < searchHistory.city.length; i++) {
         if (searchHistory.city[i].length === 3) {
             var historyEl = $("<button>").addClass("history-btn col-12 p-1 m-2").text(searchHistory.city[i][0]);
@@ -34,25 +29,25 @@ function getHistory() {
 
 function formSubmitHandler(event) {
     event.preventDefault();
-    city.name = $("#city").val().trim();
-    locate();
+    var city = $("#city").val().trim();
+    locate(city);
 }
 
-function locate() {
-    var geoLoc = "http://api.openweathermap.org/geo/1.0/direct?q=" + city.name + "&limit=5&appid=8d88d70ec92bd345dbcf4b9c1eea0ec4";
+function locate(city) {
+    var geoLoc = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=8d88d70ec92bd345dbcf4b9c1eea0ec4";
 
     fetch(geoLoc).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data);
                 if (data[0].name != undefined) {
-                    city.name = data[0].name;
-                    city.lat = data[0].lat.toString();
-                    city.lon = data[0].lon.toString();
-                    search();
+                    var name = data[0].name;
+                    var lat = data[0].lat.toString();
+                    var lon = data[0].lon.toString();
+                    search(name, lat, lon);
                 }
                 else {
-                    alert("No location found for " + city.name);
+                    alert("No location found for " + city);
                 }
             })
         }
@@ -65,21 +60,39 @@ function locate() {
         });
 }
 
-function search() {
+function search(name, lat, lon) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-        city.lat + "&lon=" + city.lon + "&exclude=minutely,hourly&appid=8d88d70ec92bd345dbcf4b9c1eea0ec4";
+        lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=8d88d70ec92bd345dbcf4b9c1eea0ec4";
 
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    searchHistory.city.pop();
+                    console.log(data);
+                    var cityStored = {
+                        index: "",
+                    };
+                    for (i = 0; i < searchHistory.city.length && !cityStored.index; i++) {
+                        if (name === searchHistory.city[i][0]) {
+                            cityStored.index = i;
+                        }
+                    }
+
+                    if (!cityStored.index) {
+                        searchHistory.city.pop();
+                    }
+                    
+                    else {
+                        searchHistory.city.splice(cityStored.index, 1);
+                    }
+
                     searchHistory.city.unshift([
-                        city.name,
-                        city.lat,
-                        city.lon
+                        name,
+                        lat,
+                        lon
                     ]);
                     localStorage.setItem("history", JSON.stringify(searchHistory));
+                    getHistory();
                 });
             }
             else {
@@ -94,3 +107,6 @@ function search() {
 getHistory();
 
 $("form").on("submit", formSubmitHandler);
+$("history").on("click", function(event) {
+    console.log(event);
+})
