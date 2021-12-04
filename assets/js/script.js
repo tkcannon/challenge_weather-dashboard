42194ce9080a8562682a73bf98d75d62be35dfde
tkcannon <taylorkcannon@gmail.com>
2021-12-04 10:58:06 -0700

    added weather conditions display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 var searchHistory = {};
 
+var searchedCity = {
+    name: '',
+    conditions: ''
+}
+
 function getHistory() {
     searchHistory = JSON.parse(localStorage.getItem("history"));
-    console.log(searchHistory);
     if (!searchHistory) {
         searchHistory = {
             city: [
@@ -29,22 +33,21 @@ function getHistory() {
 
 function formSubmitHandler(event) {
     event.preventDefault();
-    var city = $("#city").val().trim();
-    locate(city);
+    searchedCity.name = $("#city").val().trim();
+    locate();
 }
 
-function locate(city) {
-    var geoLoc = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=8d88d70ec92bd345dbcf4b9c1eea0ec4";
+function locate() {
+    var geoLoc = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchedCity.name + "&limit=5&appid=8d88d70ec92bd345dbcf4b9c1eea0ec4";
 
     fetch(geoLoc).then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-                console.log(data);
                 if (data[0].name != undefined) {
-                    var name = data[0].name;
+                    searchedCity.name = data[0].name;
                     var lat = data[0].lat.toString();
                     var lon = data[0].lon.toString();
-                    search(name, lat, lon);
+                    search(lat, lon);
                 }
                 else {
                     alert("No location found for " + city);
@@ -60,39 +63,45 @@ function locate(city) {
         });
 }
 
-function search(name, lat, lon) {
+function search(lat, lon) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-        lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=8d88d70ec92bd345dbcf4b9c1eea0ec4";
+        lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=8d88d70ec92bd345dbcf4b9c1eea0ec4";
 
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
-                    var cityStored = {
-                        index: "",
-                    };
-                    for (i = 0; i < searchHistory.city.length && !cityStored.index; i++) {
-                        if (name === searchHistory.city[i][0]) {
-                            cityStored.index = i;
+                    searchedCity.conditions = data;
+
+                    // Checks if city already exists in history
+                    var cityStoredAt = "";
+                    for (i = 0; i < searchHistory.city.length && !cityStoredAt; i++) {
+                        console.log(i);
+                        console.log(searchHistory.city[i][0]);
+                        if (searchedCity.name === searchHistory.city[i][0]) {
+                            cityStoredAt = i;
+                            console.log(cityStoredAt);
                         }
                     }
 
-                    if (!cityStored.index) {
-                        searchHistory.city.pop();
-                    }
-                    
-                    else {
-                        searchHistory.city.splice(cityStored.index, 1);
+                    if(cityStoredAt || cityStoredAt === 0) {
+                        searchHistory.city.splice(cityStoredAt, 1);
                     }
 
+                    else {  
+                        searchHistory.city.pop();
+                    }
+
+                    // Places most recent city at front of array
                     searchHistory.city.unshift([
-                        name,
+                        searchedCity.name,
                         lat,
                         lon
                     ]);
+
                     localStorage.setItem("history", JSON.stringify(searchHistory));
                     getHistory();
+                    displayConditions();
                 });
             }
             else {
@@ -104,9 +113,19 @@ function search(name, lat, lon) {
         });
 }
 
+function displayConditions() {
+    var boxEl = $("#conditions").addClass("p-1 m-1").css("border", "1px black solid");
+    boxEl.empty();
+    var nameText = $("<h1>").text(searchedCity.name + " add date here");
+    var tempText = $("<h2>").text("Temp: " + searchedCity.conditions.current.temp);
+    var windText = $("<h2>").text("Wind: " + searchedCity.conditions.current.wind_speed);
+    var humidityText = $("<h2>").text("Humidity: " + searchedCity.conditions.current.humidity);
+    var UVindexText = $("<h2>").text("UV Index: " + searchedCity.conditions.current.uvi);
+    boxEl.append(nameText, tempText, windText, humidityText, UVindexText);
+}
+
 getHistory();
 
 $("form").on("submit", formSubmitHandler);
-$("history").on("click", function(event) {
-    console.log(event);
+$("history").on("click", function (event) {
 })
