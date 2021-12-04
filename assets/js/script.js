4e03d1ffccab66dd095d42d662c56073fb51257a
tkcannon <taylorkcannon@gmail.com>
2021-12-04 11:50:55 -0700

    added 5 day forecast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,14 +65,14 @@ function locate() {
 
 function search(lat, lon) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-        lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly&appid=8d88d70ec92bd345dbcf4b9c1eea0ec4";
+        lat + "&lon=" + lon + "&units=imperial&exclude=minutely,hourly,alerts&appid=8d88d70ec92bd345dbcf4b9c1eea0ec4";
 
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
                     searchedCity.conditions = data;
-
+                    console.log(data);
                     // Checks if city already exists in history
                     var cityStoredAt = "";
                     for (i = 0; i < searchHistory.city.length && !cityStoredAt; i++) {
@@ -84,15 +84,17 @@ function search(lat, lon) {
                         }
                     }
 
-                    if(cityStoredAt || cityStoredAt === 0) {
+                    // if city is already in history, remove it
+                    if (cityStoredAt || cityStoredAt === 0) {
                         searchHistory.city.splice(cityStoredAt, 1);
                     }
 
-                    else {  
+                    // if city is not in history removes last city in history
+                    else {
                         searchHistory.city.pop();
                     }
 
-                    // Places most recent city at front of array
+                    // Places most recent searched city at front of array
                     searchHistory.city.unshift([
                         searchedCity.name,
                         lat,
@@ -114,14 +116,33 @@ function search(lat, lon) {
 }
 
 function displayConditions() {
-    var boxEl = $("#conditions").addClass("p-1 m-1").css("border", "1px black solid");
+    var boxEl = $("#conditions").addClass("p-1").css("border", "1px black solid");
     boxEl.empty();
-    var nameText = $("<h1>").text(searchedCity.name + " add date here");
-    var tempText = $("<h2>").text("Temp: " + searchedCity.conditions.current.temp);
-    var windText = $("<h2>").text("Wind: " + searchedCity.conditions.current.wind_speed);
-    var humidityText = $("<h2>").text("Humidity: " + searchedCity.conditions.current.humidity);
-    var UVindexText = $("<h2>").text("UV Index: " + searchedCity.conditions.current.uvi);
+    var nameText = $("<h2>").text(searchedCity.name + " add date here");
+    var tempText = $("<p>").text("Temp: " + searchedCity.conditions.current.temp + " F");
+    var windText = $("<p>").text("Wind: " + searchedCity.conditions.current.wind_speed + " MPH");
+    var humidityText = $("<p>").text("Humidity: " + searchedCity.conditions.current.humidity + " %");
+    var UVindexText = $("<p>").text("UV Index: " + searchedCity.conditions.current.uvi);
     boxEl.append(nameText, tempText, windText, humidityText, UVindexText);
+    displayForecast();
+}
+
+function displayForecast() {
+    var forecastEL = $("#forecast").addClass("d-flex justify-content-between row p-1");
+    var headerEl = $("<h3>").text("5-Day Forecast");
+
+    forecastEL.append(headerEl);
+
+    var daysEl = [];
+    for (i = 0; i < 5; i++) {
+        daysEl[i] = $("<div>").addClass("forecast-card col-2 p-1 m-1");
+        var dateText = $("<h4>").text("add date");
+        var tempText = ($("<p>")).text("Temp: " + searchedCity.conditions.daily[i].temp.day + " F");
+        var windText = ($("<p>")).text("Wind: " + searchedCity.conditions.daily[i].wind_speed + " MPH");
+        var humidityText = ($("<p>")).text("Humidity: " + searchedCity.conditions.daily[i].humidity + " %");
+        daysEl[i].append(dateText, tempText, windText, humidityText);
+        forecastEL.append(daysEl[i]);
+    }
 }
 
 getHistory();
